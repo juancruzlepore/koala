0aad7eef27467f2c567cc8c67ee95a94b6bc6e93
jcl <jlepore@itba.edu.ar>
2018-08-15 15:26:31 +0200

    Read last date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import {HttpService} from './http.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HttpService]
 })
 export class AppComponent {
+  lastDate: Date;
   title = 'koala';
+  constructor(private httpService: HttpService){
+    this.httpService.getLastDate().subscribe((data: string) => this.lastDate = new Date(data));
+  }
+
 }
